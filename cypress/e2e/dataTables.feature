f0f6b58f650c539a7ebcb323bb84e26965f7c566
GowriSundeep <gowrisundeep@GowriSundeeps-MacBook-Air.local>
2023-04-16 21:13:38 +0200

    dataTable feature

---
+++ cypress/e2e/dataTables.feature
@@ -0,0 +1,9 @@
+Feature: Validate then usage of data tables
+
+  I want to visit mutiple URL's 
+
+Scenario: Loading the url's
+  Given I visit this page
+   |email|   |password|
+   |student| |Password123|
+   |student| |Password123|
---
+++ cypress/e2e/dataTables/dataTables.js
@@ -0,0 +1,11 @@
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+
+Given('I visit this page', (datatable) => {
+  datatable.hashes().forEach((element) => {
+  cy.visit(Cypress.env("test"));
+     cy.get("#username").type(element.email);
+     cy.get("#password").clear();
+     cy.get("#password").type(element.password);
+     cy.get('#submit').click();
+  });
+});
